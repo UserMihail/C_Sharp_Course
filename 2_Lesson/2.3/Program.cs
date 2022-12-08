4a212a0526c1cd89d98f8952c9141d709b6536c4
Mihail <horunzhy.m@yandex.ru>
2022-12-08 19:26:53 +0300

    2_Lesson

---
+++ 2_Lesson/2.3/Program.cs
@@ -0,0 +1,27 @@
+﻿// Напишите программу, которая принимает на вход
+// число и проверяет, кратно ли оно одновременно 7 и 23
+
+/*Вариант 1
+Console.WriteLine("Введите число ");
+int a = int.Parse(Сonsole.ReadLine());
+
+if ( a % 7 ==0) 
+{
+    if ( a % 23 == 0)
+    Console.WriteLine("Число кратно 7 и 23");
+}
+else
+   Console.WriteLine("Число не кратно 7 и 23");*/
+
+   //Вариант 2(составной)
+
+   string divisible(int num)
+   {
+        if (num % 7 == 0 && num % 23 == 0)
+            return "divisible";
+        else
+            return "non-divisible";
+   } 
+   Console.WriteLine("enter number");
+   int number = int.Parse(Console.ReadLine());
+   Console.WriteLine(divisible(number));
