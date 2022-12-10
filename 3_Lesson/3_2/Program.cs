4e7719c50b50d1801e3e39b543f9dc685a9421dc
Mihail <horunzhy.m@yandex.ru>
2022-12-10 20:10:11 +0300

    lesson 3

--- 3_Lesson/3_2/Program.cs
+++ 3_Lesson/3_2/Program.cs
@@ -11,3 +11,24 @@ Console.WriteLine(Distance(3, 6, 2, 1));
 Console.WriteLine(Distance(7, -5, 1, -1));
 
 //второй варинат семинар 3 01:03:01
+
+int ReadNumber(string message)
+{
+    Console.Write(message);
+    var s = Console.ReadLine();
+    int a = s == null ? 0 : int.Parse(s);
+    return a;
+}
+double Distance(int ax, int ay, int bx, int by)
+{
+    double result;
+    result = Math.Sqrt(Math.Pow(bx - ax,2) +Math.Pow(by - ay, 2));
+    return result;
+}
+
+int ax = ReadNumber("Введите aX:");
+int ay = ReadNumber("Введите aY:");
+int bx = ReadNumber("Введите bX:");
+int by = ReadNumber("Введите bY:");
+
+Console.WriteLine(Math.Round(Distance(ax, ay, bx, by),2));
