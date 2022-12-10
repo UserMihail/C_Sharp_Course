9b40537c10cc4d3157aded21cb835e1c1a4c2031
Mihail <horunzhy.m@yandex.ru>
2022-12-10 20:41:18 +0300

    3.3

---
+++ 3_Lesson/3_3/Program.cs
@@ -0,0 +1,18 @@
+﻿// Напишите программу, которая принимает на вход число
+// (N) и выдает таблицу квадратов чисел от 1 до N.
+
+void Chet(int N)
+{
+    int i = 0;
+    double pere;
+    while (i <= N)
+    {
+        i += 1;
+        pere = Math.Pow(i,2);
+        Console.Write($"{pere},");
+    }
+
+}
+Console.WriteLine("Введите число ");
+int chislo = int.Parse(Console.ReadLine());
+Chet(chislo);
