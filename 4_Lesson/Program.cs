2cf7f7f0845e75ec870cb63b197ff80c1abe9e06
Mihail <horunzhy.m@yandex.ru>
2023-01-20 14:08:51 +0300

    Lesson 4

--- 4_Lesson/Program.cs
+++ 4_Lesson/Program.cs
@@ -1,2 +1,15 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// напишите программу которая принимает на вход число А, и 
+// выводит сумму чисел от 1 до А
+
+int SumNams(int num)
+{
+    int all_sum = 0;
+    for (int i = 1; i <= num; i++) 
+    {
+        all_sum += i;
+    }
+
+    return all_sum;
+}
+
+Console.WriteLine(SumNams(int.Parse(Console.ReadLine())));
