d2f6511cc50dcec5997dea8084bf635141b4e864
Mihail <horunzhy.m@yandex.ru>
2023-01-20 14:34:26 +0300

    4.1

---
+++ 4_Lesson/4.1/Program.cs
@@ -0,0 +1,16 @@
+﻿// Напишите программу которая принимает на вход число и выводит количество 
+// цифр в этом числе.
+
+int SumNams(int num)
+{
+    int count = 0;
+    while (num > 0)
+        {
+            num = num / 10;
+            all_sum++;
+        }
+    
+    return count;
+}
+
+Console.WriteLine(SumNams(int.Parse(Console.ReadLine())));
