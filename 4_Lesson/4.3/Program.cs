eeff9ec7600c6e59c7b1dc219886642d04f930c6
Mihail <horunzhy.m@yandex.ru>
2023-01-20 15:58:54 +0300

    4.3

---
+++ 4_Lesson/4.3/Program.cs
@@ -0,0 +1,14 @@
+﻿// Напишиет программу которая выводит массив из 8 цифр, и заполняет его 0 и 1 
+// в случайном порядке
+
+void Massiv(int num)
+{
+    int[] array = new int[num];
+    for (int i = 0; i < num; i++)
+    {
+        array[i] = new Random().Next(2);
+        Console.Write($"{array[i]} ");
+    }
+}
+Console.WriteLine("Enter a num:");
+Massiv(int.Parse(Console.ReadLine()));
